bac45ec4b9c8157a54d7a87f21756e12146262a1
Aswin VB <iit2016106@iiita.ac.in>
2020-03-22 01:18:07 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import things_to_do from './items';
 import './App.css';
+import SelectTextLoader from 'select-text-loader/dist/SelectTextLoader';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      things_to_do: things_to_do,
+      selecting: false,
+      randomIndex: -1
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  showRandom = () => {
+    this.setState({selecting: true});
+    const randomIndex = Math.round(Math.random() * (this.state.things_to_do.length - 0) + 0); 
+    this.setState({randomIndex});
+    setTimeout(()=>this.setState({selecting: false}),2000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            onClick={this.showRandom}
+          >
+            I am bored, Show a random thing to do
+          </a>
+          <p>
+            {!this.state.selecting && this.state.randomIndex!==-1?this.state.things_to_do[this.state.randomIndex]:""}
+            <SelectTextLoader loading={this.state.selecting} data={this.state.things_to_do} timeGap={60}/>
+          </p>
+        </header>
+      </div>
+    );
+    }
 }
 
 export default App;
---
+++ src/items.js
@@ -0,0 +1,32 @@
+const things_to_do =  [
+        "Laugh hystarically for no reason at all",
+        "Jump on your bed!",
+        "Draw",
+        "Dance to music that is epically epic",
+        "Teenager Post.",
+        "Text/Call your parents and tell them you are up in your room.",
+        "Text/Call your best friend and tell them you are up in your room.",
+        "Scream out of your window then quickly close the window and hide somewhere.",
+        "Play an instrument that you may have VERY LOUDLY!",
+        "Cover your floor with clothing!",
+        "Sing. Doesn't matter if you can or can not sing. Just sing your little heart out. Loudly.",
+        "Watch avengers end game!",
+        "Talk to a pillow.",
+        "Start a one person riot",
+        "Rearrange things.",
+        "Create something from random objects around your room.",
+        "Throw something out of your window. Then laugh hystarically to someone outside.",
+        "Try to find money under your bed, in your closet/dresser. If you do, yell hallelujah!",
+        "Just jam out to a song in your head.",
+        "Pretend you are on a lake. The floor is your river, the bed is your boat, and a hanger you can find with a belt on it is your fishing hook. Room fishing!",
+        "Find Narnia. Again.",
+        "Hide under your bed for 15 minutes. Then come out acting like a grizzly bear.",
+        "Invite friends over, and make them come in through the window!",
+        "Call everyone/thing around you chica",
+        "Watch birds fly in the sky and mimic them.",
+        "Text random friends, I hid the body...",
+        "Sleep",
+        "Surf the tv channels!"
+    ]
+
+export default things_to_do;
